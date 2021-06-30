2cf9bf845df0f90bc837c5ef2ae734d226acaf9e
Fabiandj07 <330852@student.mboutrecht.nl>
2021-06-30 13:50:33 +0200

    type "addition"  why " needed" "fighting sytem toegevoegd"

--- src/main/kotlin/einde.kt
+++ src/main/kotlin/einde.kt
@@ -1,46 +1,159 @@
 fun eindroom() {
-    fun eindeboss() {
 
-        var enemy2 = Enemy("gans", 10, 100, 100)
+        var weapon1 = Weapons("club", 20)
+        var enemy1 = Enemy("gans", 10, 65, 85)
         var maurice = Player("maurice", 10, 100, 105)
 
-        fun eind() {
+    fun eind() {
 
-        }
+        print("eind")
+    }
+
+    fun boss() {
+        fun vraag1() {
 
 
+            println("Waar leven ijsberen op de ......")
+            var raadsels = readLine()
+            if (raadsels == "Noordpool") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
 
-
-
-
-        fun bossgevecht() {
-            if (enemy2.speed < maurice.speed) {
-                enemy2.takeDamage(4)
-
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag1()
             }
         }
 
+
+        fun vraag2() {
+            println("Waar leven de pinguin op de .........")
+            var raadsels = readLine()
+            if (raadsels == "Zuidpool") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+            } else {
+                println("je krijgt 25 dmg")
+               maurice.takeDamage(25)
+                vraag2()
+            }
+        }
+
+        fun vraag3() {
+
+
+            println("Vraag drie: wat is de latijnse benaming van ijsberen")
+            var raadsels = readLine()
+            if (raadsels == "Ursus Maritimus") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag3()
+            }
+        }
+
+        fun vraag4() {
+
+            println("Engels naam voor ijsbeer")
+
+            var raadsels = readLine()
+            if (raadsels == "Polarbear") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("Engelsen naam voor zuidpool")
+                maurice.takeDamage(25)
+                vraag4()
+            }
+        }
+
+        fun vraag5() {
+
+            println("Engelse naam voor zuidpool")
+
+            var raadsels = readLine()
+            if (raadsels == "South pole") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag5()
+            }
+        }
+
+        fun vraag6() {
+
+            println("Kan een Pinguin vliegen")
+
+            var raadsels = readLine()
+            if (raadsels == "Nee") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag6()
+            }
+        }
+
+        fun vraag7() {
+
+            println("Kunnen struisvogels vliegen?")
+
+            var raadsels = readLine()
+            if (raadsels == "Nee") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(4)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag7()
+            }
+        }
+
+
         fun bossfight() {
             if (enemy2.hitpoints > 64) {
-                bossgevecht()
+                vraag1()
             } else if (enemy2.hitpoints > 54) {
-                bossgevecht()
+                vraag2()
 
             } else if (enemy2.hitpoints > 44) {
+                vraag3()
 
-                bossgevecht()
             } else if (enemy2.hitpoints > 34) {
+                vraag4()
 
-                bossgevecht()
+            } else if (enemy2.hitpoints > 24) {
+                vraag5()
+
+            } else if (enemy2.hitpoints > 14) {
+                vraag6()
+
+            } else if (enemy2.hitpoints > 4) {
+                vraag7()
 
             } else {
                 eind()
             }
         }
-        bossfight()
-
-
-
+       bossfight()
     }
-    eindeboss()
+    boss()
 }
--- src/main/kotlin/room1.kt
+++ src/main/kotlin/room1.kt
@@ -1,4 +1,8 @@
 fun cave(){
+
+    var weapon2 = Weapons("speer", 20)
+    var enemy2 = Enemy("beer", 10, 65, 85)
+    var maurice = Player("maurice", 10, 100, 105)
     println(" je gaat langs een meertje staat nu in de cave " +
             "er zijn wat tunnels " )
 
@@ -6,7 +10,149 @@ fun cave(){
 
     fun boss(){
 
-        println("bossfight")
+        fun vraag1() {
+
+
+            println("Waar leven ijsberen op de ......")
+            var raadsels = readLine()
+            if (raadsels == "Noordpool") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag1()
+            }
+        }
+
+
+        fun vraag2() {
+            println("Waar leven de pinguin op de .........")
+            var raadsels = readLine()
+            if (raadsels == "Zuidpool") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag2()
+            }
+        }
+
+        fun vraag3() {
+
+
+            println("Vraag drie: wat is de latijnse benaming van ijsberen")
+            var raadsels = readLine()
+            if (raadsels == "Ursus Maritimus") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag3()
+            }
+        }
+
+        fun vraag4() {
+
+            println("Engels naam voor ijsbeer")
+
+            var raadsels = readLine()
+            if (raadsels == "Polarbear") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("Engelsen naam voor zuidpool")
+                maurice.takeDamage(25)
+                vraag4()
+            }
+        }
+
+        fun vraag5() {
+
+            println("Engelse naam voor zuidpool")
+
+            var raadsels = readLine()
+            if (raadsels == "South pole") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag5()
+            }
+        }
+
+        fun vraag6() {
+
+            println("Kan een Pinguin vliegen")
+
+            var raadsels = readLine()
+            if (raadsels == "Nee") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(weapon1.damage)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag6()
+            }
+        }
+
+        fun vraag7() {
+
+            println("Kunnen struisvogels vliegen?")
+
+            var raadsels = readLine()
+            if (raadsels == "Nee") {
+                println("je hebt 20 dmg gedaan")
+                enemy2.takeDamage(4)
+                boss()
+
+            } else {
+                println("je krijgt 25 dmg")
+                maurice.takeDamage(25)
+                vraag7()
+            }
+        }
+
+
+        fun bossfight() {
+            if (enemy2.hitpoints > 64) {
+                vraag1()
+            } else if (enemy2.hitpoints > 54) {
+                vraag2()
+
+            } else if (enemy2.hitpoints > 44) {
+                vraag3()
+
+            } else if (enemy2.hitpoints > 34) {
+                vraag4()
+
+            } else if (enemy2.hitpoints > 24) {
+                vraag5()
+
+            } else if (enemy2.hitpoints > 14) {
+                vraag6()
+
+            } else if (enemy2.hitpoints > 4) {
+                vraag7()
+
+            } else {
+                eindroom()
+            }
+        }
     }
 
     fun keuzekant(){
